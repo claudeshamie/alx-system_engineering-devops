92d90eb42b3f4efc29368e18f49247d31d2bc026
claudeshamie <claudeshamie@gmail.com>
2023-04-03 14:53:59 -0700

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,53 +1,9 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+# automate the task of creating a custom HTTP header response, but with Puppet
 
-# Set up Nginx configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => "
-user www-data;
-worker_processes auto;
-pid /run/nginx.pid;
-
-events {
-    worker_connections 768;
-    # multi_accept on;
-}
-
-http {
-    sendfile on;
-    tcp_nopush on;
-    tcp_nodelay on;
-    keepalive_timeout 65;
-    types_hash_max_size 2048;
-
-    server_tokens off;
-
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        server_name _;
-
-        # Add custom HTTP header
-        add_header X-Served-By $hostname;
-
-        root /var/www/html;
-        index index.html;
-
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-    }
-}
-",
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
