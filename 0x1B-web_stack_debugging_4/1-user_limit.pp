c5cdcc0b194ddf34a2d4093b69177454e96cafd6
claudeshamie <claudeshamie@gmail.com>
2023-05-18 21:18:17 +0000

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -10,3 +10,15 @@ exec { 'increase-hard-file-limit-for-holberton-user':
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
+	'''
+	class{'::limits':
+	 limits =>{
+	  'holberton' =>{
+	    'nofile' =>{
+	      'hard' =>'50000',
+	      'soft' =>'50000',
+	     },
+	    },
+	   },
+	  },
+          ```
